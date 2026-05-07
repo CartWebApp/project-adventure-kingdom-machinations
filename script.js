e8d8c3da88f0b0c6a9e73c93bcba38cdf0334510
Rey-A-Vasquez <official.rv.7394@gmail.com>
2026-05-07 10:44:30 -0700

    random stuff

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ let currentScene = ``;
 let currentPlayer; //creating spot for player
 
 
+updatingSaveFiles();
 
 newGame.addEventListener("click", ()=>{ //if user wants a new game, go back to intro. Note: will clear local storage.
     currentScene = story.intro;
@@ -48,6 +49,11 @@ newGame.addEventListener("click", ()=>{ //if user wants a new game, go back to i
 
     generateScene(currentScene);
     advanceText(currentScene);
+})
+
+gameContinuation.addEventListener("click", ()=>{
+    document.getElementById("loadFiles").classList.remove("notActive");
+    document.getElementById("loadFiles").classList.add("loadActive");
 })
 
 gameText.addEventListener("click", () => {
@@ -290,6 +296,18 @@ export function combatOver(){
     console.log(currentScene);
     generateScene(currentScene);
     advanceText(currentScene);
+}
+
+function updatingSaveFiles(){
+    let saveFiles = JSON.parse(localStorage.getItem("savedPlayers")) || [{},{},{},{},{},{}];
+    console.log(saveFiles);
+
+    saveFiles.forEach((file, index) => {
+        console.log(file);
+        if(!file.background) return;
+        const filebackground = document.querySelector(`#save${index} > button > img`); //changing save background
+        filebackground.style.backgroundImage = `url(${file.currentScene.background})`;
+    })
 }
 
 let enemy = new Enemy("Bob", [100,10,12,15,100], ``);
